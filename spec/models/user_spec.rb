92ee3d0f2ec212daf178a431642452cd86d85ed3
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-09-14 18:14:22 -0400

    Create user model, install bcrypt

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'validations' do
+    subject { User.create(email: 'sample@email.com', password: 'abc') }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password }
+  end
+end
