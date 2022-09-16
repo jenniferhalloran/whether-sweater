d7d7ea5490529e4755fff5ee27cd364f82bba242
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-09-16 10:30:05 -0400

    Cache weather in forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::ForecastController < ApplicationController
 
   def index
     coordinates = GeocodingFacade.get_coordinates(params[:location])
-    weather = WeatherFacade.get_weather(coordinates.latitude, coordinates.longitude)
-
-    render json: ForecastSerializer.format_forecast(weather), status: :ok
+    Rails.cache.write('weather', WeatherFacade.get_weather(coordinates.latitude, coordinates.longitude))
+    cached_weather = Rails.cache.fetch('weather')
+    render json: ForecastSerializer.format_forecast(cached_weather), status: :ok
   end
 
 end
