9adae3f531d8c035b0e0b51b7ddb335ee8505589
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-09-19 11:01:49 -0400

    Change image endpoint parameter

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ImagesController < ApplicationController
   def index
-    image = ImageFacade.get_image_by_keyword(params[:location])
+    image = ImageFacade.get_image_by_keyword(params[:keyword])
     render json: ImageSerializer.new(image), status: :ok
   end
 end
--- spec/requests/api/v1/image_request_spec.rb
+++ spec/requests/api/v1/image_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Image API" do
   describe 'happy path' do
     it 'returns an image relevant to the keyword sent' do
-      get '/api/v1/images?location=denver,co' 
+      get '/api/v1/images?keyword=denver,co' 
       body = JSON.parse(response.body)
       
       expect(response).to be_successful
